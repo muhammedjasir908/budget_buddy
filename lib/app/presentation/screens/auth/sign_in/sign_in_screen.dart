2a4f4876b69996bba7e500cdbcce7c95b03c3274
jasirmohd20 <muhammedjasir908@gmail.com>
2024-03-12 15:20:04 +0400

    Auth Screens UI Completed

--- lib/app/presentation/screens/auth/sign_in/sign_in_screen.dart
+++ lib/app/presentation/screens/auth/sign_in/sign_in_screen.dart
@@ -1,8 +1,11 @@
+import 'package:budget_buddy/app/presentation/route/app_pages.dart';
 import 'package:budget_buddy/app/presentation/widgets/card_button_widget.dart';
 import 'package:budget_buddy/app/presentation/widgets/icon_card_button_widget.dart';
 import 'package:budget_buddy/app/presentation/widgets/space_widget.dart';
+import 'package:budget_buddy/res/colors.dart';
 import 'package:budget_buddy/res/images.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -12,22 +15,36 @@ class SignInScreen extends StatelessWidget {
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: darkColor,
       appBar: AppBar(
-        title: const Text('Sign In', style: TextStyle(
-          color: Colors.white,
-          fontSize: 14
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'Sign In',
+          style: TextStyle(
+          fontSize: 14,
+            color: Colors.white
         ),),
         leading: IconButton(
             onPressed: () {}, 
             icon: const Icon(
               Icons.arrow_back_rounded, 
-              size: 25,)),
+              size: 25,
+              color: Colors.white,
+            ),),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset(appImage,width: 100,height: 70,fit: BoxFit.contain,),
+            Image.asset(
+              appImage,
+              width: 100,
+              height: 70,
+              fit: BoxFit.contain,
+            ),
             const SpaceWidget(height: 10.0,),
             TextFormField(
               controller: emailController,
@@ -41,14 +58,24 @@ class SignInScreen extends StatelessWidget {
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8.0),
-                  borderSide: const BorderSide(color: Colors.grey),
+                  borderSide: const BorderSide(
+                      color: Colors.white
+                  ),
                 ),
-                prefixIcon: const Icon(Icons.email_outlined, size: 15,color: Colors.grey,),
+                prefixIcon: const Icon(
+                  Icons.email_outlined,
+                  size: 15,
+                  color: Colors.grey,
+                ),
                 hintText: 'Please enter email here',
                 hintStyle: const TextStyle(
                   fontSize: 12,
                   color: Colors.grey
-                )
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
               ),
             ),
             const SpaceWidget(height: 10.0,),
@@ -64,49 +91,68 @@ class SignInScreen extends StatelessWidget {
               decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(8.0),
-                    borderSide: const BorderSide(color: Colors.grey),
+                    borderSide: const BorderSide(color: Colors.white),
                   ),
-                  prefixIcon: const Icon(Icons.email_outlined, size: 15,color: Colors.grey,),
-                  suffix: IconButton(onPressed: () {}, icon: const Icon(Icons.visibility, size: 15,)),
+                  prefixIcon: const Icon(Icons.password_outlined, size: 15,color: Colors.white,),
+                  suffixIcon: IconButton(onPressed: () {}, icon: const Icon(Icons.visibility, size: 15,)),
                   hintText: 'Please enter password here',
                   hintStyle: const TextStyle(
                       fontSize: 12,
                       color: Colors.grey
-                  )
+                  ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
               ),
             ),
             const SpaceWidget(height: 10.0,),
-            TextButton(onPressed: () {}, child: const Text('Forgot password', style: TextStyle(
-              fontSize: 14,
+            TextButton(onPressed: () {
+              Get.toNamed(AppPages.forgotPassword);
+            }, child: const Text('Forgot password', style: TextStyle(
+              fontSize: 16,
               color: Colors.red
             ),)),
             const SpaceWidget(height: 5.0,),
-            const CardButtonWidget(buttonColor: Colors.red, text: 'LOGIN'),
-            const SpaceWidget(height: 5.0,),
+            CardButtonWidget(buttonColor: Colors.red, text: 'LOGIN',onTap: () {},),
+            const SpaceWidget(height: 15.0,),
             const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Expanded(child: Divider()),
-                Text('OR'),
-                Expanded(child: Divider())
+                Expanded(child: Divider(color: Colors.white,)),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text('or',style: TextStyle(
+                    color: Colors.white
+                  ),),
+                ),
+                Expanded(child: Divider(color: Colors.white,))
               ],
             ),
-            const SpaceWidget(height: 5.0,),
+            const SpaceWidget(height: 15.0,),
             const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(child: IconCardButtonWidget(size: 35, image: googleIcon)),
-                Expanded(child: IconCardButtonWidget(size: 35, image: facebookIcon))
+                IconCardButtonWidget(size: 35, image: googleIcon),
+                SizedBox(width:40),
+                IconCardButtonWidget(size: 35, image: facebookIcon)
               ],
             ),
-            const SpaceWidget(height: 5.0,),
-            const Divider(),
+            const SpaceWidget(height: 15.0,),
+            const Divider(color: Colors.white,),
             const SpaceWidget(height: 5.0,),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const Text("Don't have an account? ",style: TextStyle(
-                  fontSize: 14
+                  fontSize: 16,
+                  color: Colors.white
                 ),),
-                TextButton(onPressed: () {}, child: const Text('Sign Up', style: TextStyle(
-                  fontSize: 14,
+                TextButton(onPressed: () {
+                  Get.toNamed(AppPages.signUp);
+                }, child: const Text('Sign Up', style: TextStyle(
+                  fontSize: 16,
                   color: Colors.red
                 ),))
               ],
